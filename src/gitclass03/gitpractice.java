bcca96588d12f466cec17d5c1a19c5ca6e2c49df
faiza <faiza.safeer456@gmail.com>
2022-09-28 10:07:43 +1000

    third commit

--- src/gitclass03/gitpractice.java
+++ src/gitclass03/gitpractice.java
@@ -5,6 +5,8 @@ public class gitpractice {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 System.out.println("hey this is a change in my project");
+
+System.out.println("how r u with this new version");
 	}
 
 }
